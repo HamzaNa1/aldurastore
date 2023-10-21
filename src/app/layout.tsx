a23ee52b8979393746f1f6649223cb9720a866bd
HamzaN12 <hamzanasab4@gmail.com>
2023-10-21 17:44:59 +0300

    Setup layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Cairo } from "next/font/google";
@@ -16,8 +17,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
 			<body className={inter.className + " no-scrollbar"}>
+				<Navbar />
+				{children}
 			</body>
 		</html>
 	);
