2dc8416a9fba48a5b9d39cadc429e54a3679b91c
HamzaN12 <hamzanasab4@gmail.com>
2023-10-21 17:19:01 +0300

    Added colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			colors: {
+				primary: "#336A6D",
+				secondary: "#ECECEC",
+				primarytext: "#145654",
+				secondarytext: "#909090",
+			},
 		},
 	},
 	plugins: [],
