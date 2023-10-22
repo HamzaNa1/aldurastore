12a575569d07e86a00d0fc88a6c58a0523b46a75
Zakarya-Khawnda <111238130+Zakarya-Khawnda@users.noreply.github.com>
2023-10-22 20:03:31 +0300

    Update ProductView.tsx
    first code

--- src/components/ProductView.tsx
+++ src/components/ProductView.tsx
@@ -1,7 +1,30 @@
 export default function ProductView() {
 	return (
 		<>
-			<div className="w-[400px] h-[600px] bg-red-50"></div>
+			<div className="w-[400px] h-[600px] bg-red-50">
+				<div className=" w-[400px] h-[350px] bg-black">
+					<img src="" />
+				</div>
+
+				<div className="  w-[400px] h-[250px] p-4">
+					<div className="text-[#145654]   right-0 text-right text-3xl">
+						xxxxxxx
+						<br />
+						xxxxxxx
+					</div>
+
+					<div className="text-[#909090]  right-0 text-right text-xl">
+						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+						<br />
+						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+						<br />
+						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+						<br />
+						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+						<br />
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
