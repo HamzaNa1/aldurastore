53dd6aa8328027e3d51f8983ffab7554dceaf2c5
Aymanalw <140971327+Aymanalw@users.noreply.github.com>
2023-10-22 22:59:35 +0300

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,37 @@
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
 export default function Footer() {
 	return (
 		<>
-			<div className="w-full bg-secondary">FOOTER</div>
+			<div className="flex flex-col w-full h-full bg-white items-center justify-center ">
+				<div className="flex flex-row-reverse p-32 gap-40  ">
+					<div>
+						<img className="h-48 w-48" src="logoNobg.png" alt="" />
+						<div className="text-black flex flex-row gap-5 p-3 items-center justify-center">
+							<BsFacebook size={40} />
+							<BsInstagram size={40} />
+							<BsWhatsapp size={40} />
+						</div>
+					</div>
+
+					<div className=" text-black flex flex-col gap-4">
+						<span className="font-bold">الصفحات</span>
+						<span>الصفحة الرئيسية</span>
+						<span>جميع المنتجات</span>
+						<span>السلة</span>
+						<span>الحساب</span>
+						<span>English | عربي</span>
+					</div>
+
+					<div className=" text-black flex flex-col gap-4">
+						<span className="font-bold">تواصل</span>
+						<span>البريد الألكتروني</span>
+						<span>رقم الجوال</span>
+						<span>الموقع</span>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
