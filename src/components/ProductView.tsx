5e5430897ffd0aa57f44481e374ea039ec1544b7
HamzaN12 <hamzanasab4@gmail.com>
2023-10-22 20:23:14 +0300

    Update ProductView.tsx

--- src/components/ProductView.tsx
+++ src/components/ProductView.tsx
@@ -1,27 +1,19 @@
 export default function ProductView() {
 	return (
 		<>
-			<div className="w-[400px] h-[600px] bg-red-50">
-				<div className=" w-[400px] h-[350px] bg-black">
+			<div className="w-[400px] h-[700px] bg-red-50 flex flex-col">
+				<div className="w-full aspect-square bg-black">
 					<img src="" />
 				</div>
 
-				<div className="  w-[400px] h-[250px] p-4">
+				<div className=" w-full h-auto p-4 flex flex-col gap-4">
 					<div className="text-[#145654]   right-0 text-right text-3xl">
-						xxxxxxx
-						<br />
-						xxxxxxx
+						اسم المنتج اسم المنتج اسم المنتج اسم المنتج
 					</div>
 
 					<div className="text-[#909090]  right-0 text-right text-xl">
-						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-						<br />
-						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-						<br />
-						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-						<br />
-						xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-						<br />
+						وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف المنتج
+						وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف
 					</div>
 				</div>
 			</div>
