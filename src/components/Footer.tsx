7e730d8b793f25b1c1040fb6903bad31911e291a
Aymanalw <140971327+Aymanalw@users.noreply.github.com>
2023-10-23 21:39:34 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,12 @@ export default function Footer() {
 	return (
 		<>
 			<div className="flex flex-col w-full h-full bg-white items-center justify-center ">
-				<div className="flex flex-row-reverse p-32 gap-40  ">
-					<div>
-						<img className="h-48 w-48" src="logoNobg.png" alt="" />
-						<div className="text-black flex flex-row gap-5 p-3 items-center justify-center">
-							<BsFacebook size={40} />
-							<BsInstagram size={40} />
-							<BsWhatsapp size={40} />
-						</div>
+				<div className="flex flex-row p-32 gap-40  ">
+					<div className=" text-black flex flex-col gap-4">
+						<span className="font-bold">تواصل</span>
+						<span>البريد الألكتروني</span>
+						<span>رقم الجوال</span>
+						<span>الموقع</span>
 					</div>
 
 					<div className=" text-black flex flex-col gap-4">
@@ -24,11 +22,13 @@ export default function Footer() {
 						<span>English | عربي</span>
 					</div>
 
-					<div className=" text-black flex flex-col gap-4">
-						<span className="font-bold">تواصل</span>
-						<span>البريد الألكتروني</span>
-						<span>رقم الجوال</span>
-						<span>الموقع</span>
+					<div>
+						<img className="h-48 w-48" src="logoNobg.png" alt="" />
+						<div className="text-black flex flex-row gap-5 p-3 items-center justify-center">
+							<BsFacebook size={40} />
+							<BsInstagram size={40} />
+							<BsWhatsapp size={40} />
+						</div>
 					</div>
 				</div>
 			</div>
