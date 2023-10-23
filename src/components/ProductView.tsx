473652d21328e6bbc5b5d419f8766ff521e13a35
HamzaNa1 <hamzanasab4@gmail.com>
2023-10-23 17:20:16 +0300

    Product View

--- src/components/ProductView.tsx
+++ src/components/ProductView.tsx
@@ -1,7 +1,33 @@
-export default function ProductView() {
+import Image from "next/image";
+
+interface ProductViewProp {
+	imageURL: string;
+}
+
+export default function ProductView({ imageURL }: ProductViewProp) {
 	return (
 		<>
-			<div className="w-[400px] h-[600px] bg-red-50"></div>
+			<div className="w-[400px] h-[700px] bg-white flex flex-col">
+				<div className="relative w-full aspect-square">
+					<Image
+						src={imageURL}
+						alt=""
+						fill
+						className="h-full object-center object-contain"
+					/>
+				</div>
+
+				<div className="w-full h-auto p-4 flex flex-col gap-4">
+					<div className="text-[#145654] right-0 text-right text-3xl">
+						اسم المنتج اسم المنتج اسم المنتج اسم المنتج
+					</div>
+
+					<div className="text-[#909090] right-0 text-right text-xl">
+						وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف المنتج
+						وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف المنتج وصف
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
