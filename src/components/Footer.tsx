e0ce342a587e649936a886e747ff2cef89daf621
Aymanalw <140971327+Aymanalw@users.noreply.github.com>
2023-10-23 22:38:45 +0300

    adding links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { BsFacebook } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
+import Link from "next/link";
 export default function Footer() {
 	return (
 		<>
@@ -25,9 +26,41 @@ export default function Footer() {
 					<div>
 						<img className="h-48 w-48" src="logoNobg.png" alt="" />
 						<div className="text-black flex flex-row gap-5 p-3 items-center justify-center">
-							<BsFacebook size={40} />
-							<BsInstagram size={40} />
-							<BsWhatsapp size={40} />
+							<Link
+								href="https://www.facebook.com/aldurrastore?mibextid=LQQJ4d"
+								rel="noopener noreferrer"
+								target="_blank"
+								className="group"
+							>
+								<BsFacebook
+									size={40}
+									className="fill-black  group-hover:fill-primary transition duration-200"
+								/>
+							</Link>
+
+							<Link
+								href="https://instagram.com/aldurrastore?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"
+								rel="noopener noreferrer"
+								target="_blank"
+								className="group"
+							>
+								<BsInstagram
+									size={40}
+									className="fill-black  group-hover:fill-primary transition duration-200"
+								/>
+							</Link>
+
+							<Link
+								href="https://api.whatsapp.com/send?phone=96877468122"
+								rel="noopener noreferrer"
+								target="_blank"
+								className="group"
+							>
+								<BsWhatsapp
+									size={40}
+									className="fill-black  group-hover:fill-primary transition duration-200"
+								/>
+							</Link>
 						</div>
 					</div>
 				</div>
