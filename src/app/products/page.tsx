5f3e373796cf1919179e8d9afa740eede4cbf515
Zakarya-Khawnda <111238130+Zakarya-Khawnda@users.noreply.github.com>
2023-10-24 18:00:16 +0300

    products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,28 @@
+import ProductView from "@/components/ProductView";
+const image = "/test.png";
+
 export default function Products() {
-	return <></>;
+	const images = [
+		"/logo.png",
+		"/logo.png",
+		"/logo.png ",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+	];
+
+	return (
+		<>
+			<div className="w-full h-fit bg-secondary flex flex-row flex-wrap justify-start gap-10 px-80 ">
+				{images.map((image) => (
+					<ProductView imageURL={image}></ProductView>
+				))}
+				;
+			</div>
+		</>
+	);
 }
