18cdfad6fc520b954919756fc25096ffb5a694fd
HamzaN12 <hamzanasab4@gmail.com>
2023-10-25 14:09:37 +0300

    Fixed UI bug

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,4 @@
 import ProductView from "@/components/ProductView";
-const image = "/test.png";
 
 export default function Products() {
 	const images = [
@@ -11,17 +10,14 @@ export default function Products() {
 		"/test.png",
 		"/test.png",
 		"/test.png",
-		"/test.png",
-		"/test.png",
 	];
 
 	return (
 		<>
 			<div className="w-full h-fit bg-secondary flex flex-row flex-wrap justify-start gap-10 px-80 ">
-				{images.map((image) => (
-					<ProductView imageURL={image}></ProductView>
+				{images.map((image, i) => (
+					<ProductView key={i} imageURL={image}></ProductView>
 				))}
-				;
 			</div>
 		</>
 	);
