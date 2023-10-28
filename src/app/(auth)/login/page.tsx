8199caf0c262a5547789deed1519b9ceeb946445
HamzaN12 <hamzanasab4@gmail.com>
2023-10-28 13:51:43 +0300

    Added auth pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,53 @@
+import SignIn from "@/actions/auth/SignIn";
+import { getServerSession } from "@/lib/userUtils";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Login() {
+	const user = getServerSession();
+
+	if (user) {
+		redirect("/");
+	}
+
+	async function login(formData: FormData) {
+		"use server";
+		const email = formData.get("email") as string;
+		const password = formData.get("password") as string;
+
+		await SignIn({ email, password });
+	}
+
+	return (
+		<div className="w-full flex flex-col items-center bg-secondary p-32 gap-2">
+			<form
+				className="container flex flex-col w-64 justify-center items-center gap-6"
+				action={login}
+			>
+				<input
+					className="w-full h-10 p-1 rounded-sm placeholder:text-right text-black"
+					placeholder="البريد الألكتروني"
+					type="email"
+					name="email"
+					required
+				/>
+				<input
+					className="w-full h-10 p-1 rounded-sm placeholder:text-right text-black"
+					placeholder="كلمة السر"
+					type="password"
+					name="password"
+					required
+				/>
+				<button
+					className="w-full h-10 p-1 rounded-sm drop-shadow-md bg-primary disabled:bg-primarytext text-white"
+					type="submit"
+				>
+					تسجيل الدخول
+				</button>
+			</form>
+			<Link className="text-black text-right w-64" href="/register">
+				تسجيل حساب جديد
+			</Link>
+		</div>
+	);
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,62 @@
+import SignIn from "@/actions/auth/SignIn";
+import SignUp from "@/actions/auth/SignUp";
+import { getServerSession } from "@/lib/userUtils";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Login() {
+	const user = getServerSession();
+
+	if (user) {
+		redirect("/");
+	}
+
+	async function register(formData: FormData) {
+		"use server";
+		const name = formData.get("name") as string;
+		const email = formData.get("email") as string;
+		const password = formData.get("password") as string;
+
+		await SignUp({ name, email, password });
+	}
+
+	return (
+		<div className="w-full flex flex-col items-center bg-secondary p-32 gap-2">
+			<form
+				className="container flex flex-col w-64 justify-center items-center gap-6"
+				action={register}
+			>
+				<input
+					className="w-full h-10 p-1 rounded-sm placeholder:text-right text-black"
+					placeholder="الأسم"
+					type="text"
+					name="name"
+					required
+				/>
+				<input
+					className="w-full h-10 p-1 rounded-sm placeholder:text-right text-black"
+					placeholder="البريد الألكتروني"
+					type="email"
+					name="email"
+					required
+				/>
+				<input
+					className="w-full h-10 p-1 rounded-sm placeholder:text-right text-black"
+					placeholder="كلمة السر"
+					type="password"
+					name="password"
+					required
+				/>
+				<button
+					className="w-full h-10 p-1 rounded-sm drop-shadow-md bg-primary disabled:bg-primarytext text-white"
+					type="submit"
+				>
+					تسجيل
+				</button>
+			</form>
+			<Link className="text-black text-right w-64" href="/login">
+				لديك حساب؟
+			</Link>
+		</div>
+	);
+}
