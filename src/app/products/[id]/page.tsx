951e815746ae1582488c579e0e476b6568eacc36
HamzaN12 <hamzanasab4@gmail.com>
2023-11-02 18:49:48 +0300

    Added image slider

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,5 @@
+import ProductImageSlider from "@/components/ProductImageSlider";
+
 interface ProductPageProps {
 	params: {
 		id: string;
@@ -5,38 +7,72 @@ interface ProductPageProps {
 }
 
 export default function Product({ params: { id } }: ProductPageProps) {
-	const colors = ["1", "2", "3"];
+	const colors = ["F8F8F8", "232323", "A9B0AA"];
 	const sizes = ["S", "M", "L", "XL"];
+	const images = [
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+		"/test.png",
+	];
+
 	return (
-		<div>
-			<div className="bg-secondary w-full h-fit flex flex-row justify-center p-[60px]">
-				<div className="bg-secondary w-full h-[800px] flex justify-center items-center">
-					<div className="w-[780px] h-[780px] bg-black"></div>
-				</div>
-				<div className="bg-secondary w-full h-[800px] ">
-					<div className="w-full h-fit text-right font-bold text-[32px]">
-						<span className="text-primary ">
-							xxxxxxxxxxxx <br />
-							xxxxxxxxxxxx
+		<div className="bg-secondary w-full h-fit flex flex-row justify-center p-[60px]">
+			<ProductImageSlider imageUrls={images}></ProductImageSlider>
+			<div className="w-full flex flex-col gap-20 items-end">
+				<div className="w-full flex flex-col gap-2">
+					<div className="w-full text-right font-bold text-3xl">
+						<span className="text-primary">
+							اسم المنتج اسم المنتج اسم المنتج اسم المنتج
 						</span>
 					</div>
-					<div className="h-[3px] w-full bg-primary "></div>
-					<div className="w-full h-fit text-right text-[20px]">
-						<span className="text-black">9.99$</span>
+					<div className="h-[2px] w-full bg-zinc-800 mx-2"></div>
+					<div className="w-full h-fit text-right text-xl">
+						<span className="text-zinc-800">$9.99</span>
 					</div>
+				</div>
 
-					<div className="text-black text-right py-60">
-						<div>
-							<span>:المقاس</span>
-							<div className=""></div>
+				<div className="flex flex-col gap-5 text-zinc-800 text-right">
+					<div className="flex flex-col gap-2">
+						<span className="font-bold text-xl">:اللون</span>
+						<div className="flex flex-row gap-2 justify-end">
+							{colors.map((color, i) => (
+								<button
+									key={i}
+									className="w-8 h-8 rounded-md outline-black outline-[1px] outline hover:brightness-90 transition duration-300"
+									style={{ backgroundColor: `#${color}` }}
+								></button>
+							))}
 						</div>
-						<div>
-							<span>:اللون</span>
+					</div>
+					<div className="flex flex-col gap-2">
+						<span className="font-bold text-xl">:المقاس</span>
+						<div className="flex flex-row gap-2 justify-end">
+							{sizes.map((size, i) => (
+								<button
+									key={i}
+									className="w-16 rounded-md bg-[#D9D9D9] py-1 hover:brightness-90 transition duration-300"
+								>
+									{size}
+								</button>
+							))}
 						</div>
 					</div>
 				</div>
+
+				<div className="flex flex-row gap-10">
+					<button className="py-2 px-12 bg-primary rounded-md drop-shadow-lg hover:brightness-90 transition duration-300">
+						أضف إلى السلة
+					</button>
+				</div>
 			</div>
-			<span className="text-black">{id}</span>;
 		</div>
 	);
 }
---
+++ src/components/ProductImageSlider.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import { useEffect, useState } from "react";
+
+interface ProductImageSliderProps {
+	imageUrls: string[];
+}
+
+export default function ProductImageSlider({
+	imageUrls,
+}: ProductImageSliderProps) {
+	const [loaded, setLoaded] = useState(false);
+	const [currentImage, setCurrentImage] = useState(0);
+
+	const [sliderRef, instanceRef] = useKeenSlider(
+		{
+			vertical: true,
+			mode: "free",
+			slides: { perView: "auto", spacing: 5 },
+		},
+		[]
+	);
+
+	useEffect(() => {
+		setLoaded(true);
+	}, []);
+
+	return (
+		<div className="relative w-full h-fit flex p-10 gap-3">
+			<div className="relative w-[13%] overflow-hidden">
+				{loaded ? (
+					<div className="absolute w-full h-full">
+						<div ref={sliderRef} className="relative w-full h-full">
+							{imageUrls.map((url, i) => (
+								<button
+									key={i}
+									className="keen-slider__slide"
+									onClick={() => {
+										setCurrentImage(i);
+									}}
+								>
+									<div className="w-full aspect-square bg-white">
+										<img
+											className="h-full object-center object-contain mx-auto"
+											src={url}
+										></img>
+									</div>
+								</button>
+							))}
+						</div>
+					</div>
+				) : (
+					<div className="flex flex-col gap-3">
+						<div className="w-full aspect-square bg-white"></div>
+						<div className="w-full aspect-square bg-white"></div>
+						<div className="w-full aspect-square bg-white"></div>
+						<div className="w-full aspect-square bg-white"></div>
+						<div className="w-full aspect-square bg-white"></div>
+					</div>
+				)}
+			</div>
+			<div className="w-full aspect-square bg-white">
+				<img
+					className="h-full object-center object-contain mx-auto"
+					src={imageUrls[currentImage]}
+				></img>
+			</div>
+		</div>
+	);
+}
