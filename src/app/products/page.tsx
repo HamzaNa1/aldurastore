48588e4e2333318f7121fbd4a6160acf38cbf38e
HamzaN12 <hamzanasab4@gmail.com>
2023-11-06 14:42:46 +0300

    Added Filters

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -14,11 +14,26 @@ export default function Products() {
 
 	return (
 		<>
-			<div className="w-full h-fit bg-secondary flex flex-row flex-wrap justify-start gap-10 px-80 ">
+			<div className="flex justify-center w-full bg-secondary brightness-90 p-2">
+				<div className="container flex flex-row flex-wrap gap-3 justify-center">
+					{Filter("Woman")}
+					{Filter("Men")}
+					{Filter("Children")}
+				</div>
+			</div>
+			<div className="w-full h-fit bg-secondary flex flex-row flex-wrap justify-start gap-10 px-80">
 				{images.map((image, i) => (
 					<ProductView key={i} imageURL={image}></ProductView>
 				))}
 			</div>
 		</>
+	);
+}
+
+function Filter(filterName: string) {
+	return (
+		<button className="w-24 py-2 bg-white rounded-full text-black">
+			{filterName}
+		</button>
 	);
 }
