8e68b6e1efefec3f41b8ce17bdd589a444e293c1
HamzaN12 <hamzanasab4@gmail.com>
2023-11-09 13:30:43 +0300

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,3 @@
-import ProductBannerView from "@/components/ProductBannerView";
 import { Product } from "@/lib/schema";
 
 export default function Cart() {
@@ -11,23 +10,27 @@ export default function Cart() {
 		activated: true,
 	};
 
+	const products = [testProduct, testProduct, testProduct];
+
 	return (
 		<>
-			<div className="flex flex-row w-full h-full bg-secondary px-64 justify-center items-center gap-16 p-20">
-				<div className="flex flex-col h-full flex-grow gap-7">
-					<span className="text-black text-3xl font-bold text-right w-full h-10 justify-end">
+			<div className="flex flex-row w-full h-full bg-secondary px-64 justify-center gap-16 p-20">
+				<div className="flex flex-col h-full flex-grow gap-3">
+					<span className="text-black text-3xl font-bold text-right w-full h-10">
 						تفاصيل الفاتورة
 					</span>
-					<div className="flex flex-row h-7 w-full outline rounded-sm p-1 bg-zinc-400 outline-zinc-400">
-						<span className="text-black text-sm text-right flex-grow">
-							القيمة
-						</span>
-						<span className="text-black text-sm text-right flex-grow">
-							الوصف
-						</span>
-					</div>
-					<div className="flex flex-row h-7 w-full outline rounded-sm p-3 bg-white outline-zinc-400"></div>
-					<div className="flex flex-row h-7 w-full outline rounded-sm p-3 bg-white outline-zinc-400"></div>
+					<table className="table-fixed border-separate border-spacing-y-6">
+						<thead className="text-black text-sm text-right outline outline-zinc-400 bg-zinc-400 rounded-sm">
+							<th>القيمة</th>
+							<th>الوصف</th>
+						</thead>
+						<tbody>
+							<tr className="text-black text-sm text-right outline outline-zinc-400 bg-white rounded-sm">
+								<td className="pr-1">0</td>
+								<td className="pr-1">10</td>
+							</tr>
+						</tbody>
+					</table>
 					<button className="bg-black h-7 w-full rounded-md">
 						متابعة الشراء
 					</button>
@@ -35,45 +38,41 @@ export default function Cart() {
 						العودة للتسوق
 					</button>
 				</div>
-				{/*left col */}
-				<div className="flex flex-col h-full flex-grow-0">
-					<span className="text-black text-3xl font-bold text-right w-full h-10 justify-center">
+				<div className="flex flex-col h-full flex-grow-0 min-w-[60%] gap-7">
+					<span className="text-black text-3xl font-bold text-right w-full h-10">
 						المنتجات
 					</span>
-					<div className="flex flex-row h-7 w-full outline rounded-sm p-1 bg-zinc-400 outline-zinc-400">
-						<span className="text-black text-sm text-start flex-grow">
-							السعر
-						</span>
-						<span className="text-black text-sm text-center flex-grow">
-							الكمية
-						</span>
-						<span className="text-black text-sm text-end flex-grow">
-							المنتج
-						</span>
-					</div>
-
-					<ProductBannerView
-						id={testProduct.id}
-						name={testProduct.name}
-						description={testProduct.description}
-						imageURL={testProduct.imageURL}
-					></ProductBannerView>
-					<ProductBannerView
-						id={testProduct.id}
-						name={testProduct.name}
-						description={testProduct.description}
-						imageURL={testProduct.imageURL}
-					></ProductBannerView>
-					<ProductBannerView
-						id={testProduct.id}
-						name={testProduct.name}
-						description={testProduct.description}
-						imageURL={testProduct.imageURL}
-					></ProductBannerView>
+					<table className="table-auto">
+						<thead className="text-zinc-50 text-sm text-right outline rounded-sm bg-zinc-400 outline-zinc-400 h-fit">
+							<td className="w-[7.5%]"></td>
+							<td className="py-1">السعر</td>
+							<td>الكمية</td>
+							<td>المنتج</td>
+						</thead>
+						<tbody>
+							{products.map((product, i) => (
+								<tr
+									key={i}
+									className="text-zinc-800 text-sm text-right outline rounded-sm bg-white outline-zinc-400 h-fit"
+								>
+									<div className="relative flex items-center justify-center h-full aspect-square">
+										<div className="absolute w-full h-full p-1 flex justify-center">
+											<img
+												src={product.imageURL}
+												alt=""
+												className="h-full object-center object-contain"
+											/>
+										</div>
+									</div>
+									<td className="pr-1">{10}</td>
+									<td className="pr-1">{10}</td>
+									<td className="pr-1 py-2">{product.name}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</div>
-				{/*right col */}
 			</div>
-			{/* biggest div */}
 		</>
 	);
 }
