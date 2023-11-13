c7ed8664c8ec712a9eedf2a69642b15a798d6225
HamzaN12 <hamzanasab4@gmail.com>
2023-11-13 16:54:54 +0300

    Added delete item action

--- src/actions/Cart.ts
+++ src/actions/Cart.ts
@@ -1,14 +1,11 @@
 "use server";
 
 import db from "@/lib/db";
-import { cartItems, products } from "@/lib/schema";
+import { cartItems } from "@/lib/schema";
 import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
-export async function GetCartByUser(userId: string) {
-	const result = await db
-		.select()
-		.from(cartItems)
-		.innerJoin(products, eq(cartItems.productId, products.id));
-
-	return result;
+export async function DeleteItem(cartItemId: string) {
+	await db.delete(cartItems).where(eq(cartItems.id, cartItemId));
+	revalidatePath("/");
 }
