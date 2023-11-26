0efc2dd5a8d1c6b7372782662e0e80d2d49b5a32
HamzaN12 <hamzanasab4@gmail.com>
2023-11-26 17:28:08 +0300

    Bug fixed

--- src/lib/Utils/emailUtils.ts
+++ src/lib/Utils/emailUtils.ts
@@ -6,12 +6,12 @@ const resend = new Resend(process.env.RESEND_KEY);
 export default async function sendEmail(
 	to: string,
 	title: string,
-	email: React.JSX.Element
+	email: string
 ) {
 	await resend.emails.send({
 		from: "متجر الدرة <noreply@aldurastore.com>",
 		to: to,
 		subject: title,
-		react: email,
+		html: email,
 	});
 }
