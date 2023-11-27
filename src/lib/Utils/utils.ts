28377ce161ed56953405d753073b52f56ef7c73c
HamzaN12 <hamzanasab4@gmail.com>
2023-11-27 13:25:00 +0300

    Small changes

--- src/lib/Utils/utils.ts
+++ src/lib/Utils/utils.ts
@@ -2,14 +2,15 @@ export function isEmailValid(email: string) {
 	const emailRegex =
 		/^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
-	if (!email) return false;
-
-	if (email.length > 254) return false;
+	if (!email || email.length > 254) {
+		return false;
+	}
 
 	var valid = emailRegex.test(email);
-	if (!valid) return false;
+	if (!valid) {
+		return false;
+	}
 
-	// Further checking of some things regex can't handle
 	var parts = email.split("@");
 	if (parts[0].length > 64) return false;
 
@@ -18,8 +19,9 @@ export function isEmailValid(email: string) {
 		domainParts.some(function (part) {
 			return part.length > 63;
 		})
-	)
+	) {
 		return false;
+	}
 
 	return true;
 }
