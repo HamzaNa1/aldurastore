e8236721382a709001ac5ab8aa3340b956e14e97
HamzaN12 <hamzanasab4@gmail.com>
2024-01-11 11:09:46 +0300

    Update SubmitButton.tsx

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -1,24 +1,26 @@
 "use client";
 
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 import { useFormStatus } from "react-dom";
 
+interface SubmitButtonProps
+	extends DetailedHTMLProps<
+		ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	> {
+	fallback?: React.ReactNode;
+}
+
 export function SubmitButton({
-	className,
 	fallback,
 	children,
-}: {
-	className?: string;
-	fallback?: React.ReactNode;
-	children?: React.ReactNode;
-}) {
+	disabled,
+	...props
+}: SubmitButtonProps) {
 	const { pending } = useFormStatus();
 
 	return (
-		<button
-			type="submit"
-			className={className + " disabled:cursor-pointer"}
-			disabled={pending}
-		>
+		<button {...props} disabled={disabled || pending}>
 			{pending ? fallback ?? children : children}
 		</button>
 	);
