6a3823cae8c1d63a8c44c0138b91888b39d1c082
HamzaN12 <hamzanasab4@gmail.com>
2024-03-28 15:53:25 +0300

    fix db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,8 @@
-// import "server-only";
-
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
 
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
 	database: "railway",
 	host: process.env["DATABASE_HOST"],
 	user: process.env["DATABASE_USERNAME"],
